cf9ecdf8599c0433542db28650d8db4488da04c1
jeong67jea <jeongjae_choi@naver.com>
2025-10-05 16:53:09 +0900

    Add files via upload

---
+++ mobile-fit.js
@@ -0,0 +1,3 @@
+(function(){try{window.mobileFit=window.mobileFit||{};
+if(typeof window.mobileFit.init!=='function')window.mobileFit.init=function(){};
+if(typeof window.mobileFit.setScale!=='function')window.mobileFit.setScale=function(){};}catch(e){}})();
