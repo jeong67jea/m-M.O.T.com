fdb6839b7a66ace5dd668ca0e367592f424b2227
jeong67jea <jeongjae_choi@naver.com>
2025-10-16 17:05:00 +0800

    Delete cn directory

--- cn/mobile-fit.js
+++
@@ -1,3 +0,0 @@
-(function(){try{window.mobileFit=window.mobileFit||{};
-if(typeof window.mobileFit.init!=='function')window.mobileFit.init=function(){};
-if(typeof window.mobileFit.setScale!=='function')window.mobileFit.setScale=function(){};}catch(e){}})();
